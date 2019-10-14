1bfb69afae0ab0168d7421d879cd8746172f447c
hhu94 <haohu94@gmail.com>
2019-10-13 21:06:58 -0700

    Updates

---
+++ precache-manifest.ec3000fb7bc376c4f648fe81b2930f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c3e02db17aa6eedc32e6adbeaa13b4",
+    "url": "/listennit/index.html"
+  },
+  {
+    "revision": "225ce2f4e0d36b8bdebe",
+    "url": "/listennit/static/css/main.0154f281.chunk.css"
+  },
+  {
+    "revision": "81a64e85ff86e3338012",
+    "url": "/listennit/static/js/2.70cf370c.chunk.js"
+  },
+  {
+    "revision": "225ce2f4e0d36b8bdebe",
+    "url": "/listennit/static/js/main.9df109aa.chunk.js"
+  },
+  {
+    "revision": "75802d9002b6c1692596",
+    "url": "/listennit/static/js/runtime-main.34461cda.js"
+  }
+]);
