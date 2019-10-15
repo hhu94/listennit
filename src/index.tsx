d022237ae9d655905e1eead268b0df9daa1836fd
hhu94 <haohu94@gmail.com>
2019-10-15 00:58:12 -0700

    Configure Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
+import * as Sentry from "@sentry/browser";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  release: process.env.REACT_APP_COMMIT_ID
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
