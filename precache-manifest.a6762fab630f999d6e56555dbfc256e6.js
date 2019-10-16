52454132ce050609d5c821a52e1002e2ec4e5caa
hhu94 <hhu94@users.noreply.github.com>
2019-10-16 03:29:51 +0000

    Automated deployment: Wed Oct 16 03:29:51 UTC 2019 50ea712bd8fd5effbb79201488645e1a4ca6e550

--- precache-manifest.a6762fab630f999d6e56555dbfc256e6.js
+++ precache-manifest.a6762fab630f999d6e56555dbfc256e6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e98ac320cd3e6f1522ba8f5500702e4d",
+    "revision": "6d723398ca9274b7de727d1fbbb06805",
     "url": "/index.html"
   },
   {
-    "revision": "12a86d0a83036327f76e",
+    "revision": "4eee4534943d191e78b3",
     "url": "/static/css/main.d8faec2d.chunk.css"
   },
   {
-    "revision": "ca0fbe3369f2b9d53f3d",
-    "url": "/static/js/2.f5c01ac5.chunk.js"
+    "revision": "9f42afcb413d472450f4",
+    "url": "/static/js/2.d2c9fa09.chunk.js"
   },
   {
-    "revision": "12a86d0a83036327f76e",
-    "url": "/static/js/main.09401d92.chunk.js"
+    "revision": "4eee4534943d191e78b3",
+    "url": "/static/js/main.4fac7b8d.chunk.js"
   },
   {
     "revision": "40f5db90fc990e775717",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bcb6b472a48411f7c06d65cf3cde70a.js"
+  "/precache-manifest.a6762fab630f999d6e56555dbfc256e6.js"
 );
 
 self.addEventListener('message', (event) => {
